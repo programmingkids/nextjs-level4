2ddf12374a2ad5e10bcfb227b2181000f8529cf8
Hiroshi Fujiwara <fujiwara@no1s.biz>
2024-10-02 05:34:45 +0000

    fix chapter0$

--- chapter04/src/app/orange/page.tsx
+++ chapter04/src/app/orange/page.tsx
@@ -1,20 +1,20 @@
-import { cookies } from "next/headers";
+import { cookies } from 'next/headers';
 
 export default function Page() {
-  console.log("This is orange page");
+  console.log('This is orange page');
   // 以下の3行はダイナミックレンダリングにするための処理
   // 処理内容自体には大きな意味はない
   const cookieStore = cookies();
-  const value = cookieStore.get("title");
+  const value = cookieStore.get('title');
   console.log(value); // undefined
 
   return (
     <div className="main">
       <h1 className="p-4 bg-orange-200 text-xl">Orange</h1>
       <div className="py-4 px-6">
-        <div className="py-4 text-start">{"orange, ".repeat(20)}</div>
+        <div className="py-4 text-start">{'orange, '.repeat(20)}</div>
         <div>
-          {new Date().toLocaleTimeString("ja-JP", { timeZone: "Asia/Tokyo" })}
+          {new Date().toLocaleTimeString('ja-JP', { timeZone: 'Asia/Tokyo' })}
         </div>
       </div>
     </div>
